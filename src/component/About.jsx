11619f02c9abfe848e7dc35d8abdce02284e02ff
sainipankaj12 <125671488+sainipankaj12@users.noreply.github.com>
2023-11-22 01:02:35 +0530

    twelth commit

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -11,7 +11,7 @@ function About() {
         
           <Card className=" text-white">
             <Card.Img
-              src="./Img/girl-shope.PNG "
+              src="./Img/shoppy.PNG "
               name = "imgs"
               alt="about.jpg"
               height="550px"
